203d7665defc314f7f7fe85e3abc08a5cb8e2708
Sirfalus <rvivid@gmail.com>
2017-08-13 20:44:25 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -21,7 +21,7 @@ if($arrJson['events'][0]['message']['text'] == "สวัสดี"){
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันยังไม่มีชื่อนะ";
-}else if($arrJson['events'][0]['message']['text'] == "ทำอะไรได้บ้าง"){
+}else if(similar_text($arrJson['events'][0]['message']['text'],"ทำอะไรได้บ้าง" >=10)){
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
